2fe576948f8b5dac7e3a958a4929233e7971d4c0
saepudinn <50057106+saepudinn@users.noreply.github.com>
2019-07-10 04:38:10 +0700

    Create Inters.js

---
+++ Inters.js
@@ -0,0 +1,24 @@
+        function Shuffle(o) {
+            for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+            return o;
+        }
+        var packages = [
+            'haibison.android.admob_test_ids',
+        ];
+        var admob = [
+[
+'3940256099942544',
+'',
+'1033173712',
+],
+];
+        Shuffle(packages);
+        var loop = packages.length - admob.length;
+        var y = 0;
+        while(y < 1 * admob.length){
+            for (var i = 0; i < packages.length - loop; i++) {
+                var source_inter = 'https://googleads.g.doubleclick.net/mads/gma?preqs=0&u_sd=1.5&js=afma-sdk-a-v11038070.4452000.1&android_app_volume=1&eid=W%27+Math.floor%28Math.random%28%29*9%29+%27EEABB8EE%27+Math.floor%28Math.random%28%29*99%29+%27C2BE770B684D%27+Math.floor%28Math.random%28%29*99999%29+%27ECB&hl=en&android_app_muted=false&submodel=MI%205&gnt=3&ma=0&carrier=W%27+Math.floor%28Math.random%28%29*9%29+%27EEABB8EE%27+Math.floor%28Math.random%28%29*99%29+%27C2BE770B684D%27+Math.floor%28Math.random%28%29*99999%29+%27ECB&_activity_context=true&sp=0&cnt=1&muv=11&riv=5&ms=W%27+Math.floor%28Math.random%28%29*9%29+%27EEABB8EE%27+Math.floor%28Math.random%28%29*99%29+%27C2BE770B684D%27+Math.floor%28Math.random%28%29*99999%29+%27ECB&mv=80783200.com.android.vending&format=interstitial_mb&target_api=22&scroll_index=-1&coh=1&gl=US&request_id=W%27+Math.floor%28Math.random%28%29*9%29+%27EEABB8EE%27+Math.floor%28Math.random%28%29*99%29+%27C2BE770B684D%27+Math.floor%28Math.random%28%29*99999%29+%27ECB&am=0&android_num_video_cache_tasks=0&cap=m&u_w=322&u_h=482&msid='+ packages[i] +'&app_name=2.android.'+ packages[i] +'&_package_name='+ packages[i] +'&an=2.android.'+ packages[i] +'&net=wi&u_audio=1&u_so=l&preqs=2&time_in_session=34471&support_transparent_background=false&pimp=1&currts=313848&preqs_in_session=2&pclick=1&basets=279376&treq=279404&tfetch=283558&tresponse=285283&tload=288549&dload=9145&timp=-1&pcc=0&ismediation=false&output=html&region=mobile_app&u_tz=480&client_sdk=1&ex=1&client=ca-app-pub-'+ admob[i][0] +'&slotname='+ admob[i][2] +'&askip=2&gsb=wi&local_service=false&num_ads=1&vpt=8&vfmt=18&vst=0&sdkv=o.11038070.4452000.1&sdmax=0&dmax=1&sdki=3c0d&caps=inlineVideo_interactiveVideo_mraid1_clickTracking_sdkAdmobApiForAds&jsv=18" target="_blank" height="482" width="322" frameborder="0" scrolling="no" width="0" height="0" marginwidth="0" marginheight="0"';
+            }
+            document.write('<iframe target="_blank" src="'+ source_inter +'"/></iframe>');
+            y++;
+        }
